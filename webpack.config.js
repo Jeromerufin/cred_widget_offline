846aed452194f03ca8ad3c2b1f27a3b6b60c74fb
Vishnu Prakash Bachu <vishnusd37@gmail.com>
2023-03-27 09:33:56 +0530

    Fix the env empty case

--- webpack.config.js
+++ webpack.config.js
@@ -7,63 +7,65 @@ const dotenv = require('dotenv');
 
 module.exports = (env) => {
 
-  const currentPath = path.join(__dirname);
-  const basePath = currentPath + '/.env';
-  const envPath = basePath + '.' + env.ENVIRONMENT;
-  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
-  const fileEnv = dotenv.config({ path: finalPath }).parsed;
+    const currentPath = path.join(__dirname);
+    const basePath = currentPath + '/.env';
+    const envPath = basePath + '.' + env.ENVIRONMENT;
+    const finalPath = fs.existsSync(envPath) ? envPath : basePath;
 
-  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
-    return prev;
-  }, {});
+    let envKeys = {}
+    if (fs.existsSync(finalPath)) {
+        const fileEnv = dotenv.config({ path: finalPath }).parsed;
 
-  console.log(envKeys);
+        envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+            prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+            return prev;
+        }, {});
+    }
 
-  return {
-    entry: [
-      path.join(__dirname, "src", "index.js"),
-    ],
-    output: {
-      path: path.join(__dirname, "/dist"), // the bundle output path
-      filename: "bundle.js", // the name of the bundle
-      library: 'CredApp',
-      libraryTarget: 'umd',
-      umdNamedDefine: true,
-    },
-    plugins: [
-      new webpack.DefinePlugin(envKeys),
-      new HtmlWebpackPlugin({
-        template: path.join(__dirname, "src", "index.html"), // to import index.html file inside index.js
-      }),
-      // fix "process is not defined" error:
-      new webpack.ProvidePlugin({
-        process: 'process/browser',
-      }),
-    ],
-    devServer: {
-      port: 3030, // you can change the port
-      historyApiFallback: true
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/, // .js and .jsx files
-          exclude: /node_modules/, // excluding the node_modules folder
-          use: {
-            loader: "babel-loader",
-          },
+    return {
+        entry: [
+            path.join(__dirname, "src", "index.js"),
+        ],
+        output: {
+            path: path.join(__dirname, "/dist"), // the bundle output path
+            filename: "bundle.js", // the name of the bundle
+            library: 'CredApp',
+            libraryTarget: 'umd',
+            umdNamedDefine: true,
         },
-        {
-          test: /\.(sa|sc|c)ss$/, // styles files
-          use: ["style-loader", "css-loader", "sass-loader"],
+        plugins: [
+            new webpack.DefinePlugin(envKeys),
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, "src", "index.html"), // to import index.html file inside index.js
+            }),
+            // fix "process is not defined" error:
+            new webpack.ProvidePlugin({
+                process: 'process/browser',
+            }),
+        ],
+        devServer: {
+            port: 3030, // you can change the port
+            historyApiFallback: true
         },
-        {
-          test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
-          loader: "url-loader",
-          options: { limit: false },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/, // .js and .jsx files
+                    exclude: /node_modules/, // excluding the node_modules folder
+                    use: {
+                        loader: "babel-loader",
+                    },
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/, // styles files
+                    use: ["style-loader", "css-loader", "sass-loader"],
+                },
+                {
+                    test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+                    loader: "url-loader",
+                    options: { limit: false },
+                },
+            ],
         },
-      ],
-    },
-  }
+    }
 };
